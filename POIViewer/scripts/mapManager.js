b2833c6ac4b8936fc8c9eab1c0d7dc133590407b
kurokocaca <michel.huynh@outlook.com>
2026-04-20 23:24:14 +0200

    déplacement à gauche changement de fond de carte

--- POIViewer/scripts/mapManager.js
+++ POIViewer/scripts/mapManager.js
@@ -107,28 +107,23 @@ export class MapManager {
 
     addToggleControl() {
         const ToggleControl = L.Control.extend({
-            options: { position: 'topright' },
+            options: { position: 'topleft' },
 
             onAdd: (map) => {
-                const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
-                container.style.backgroundColor = 'white';
-                container.style.width = '30px';
-                container.style.height = '30px';
-                container.style.cursor = 'pointer';
-                container.style.display = 'flex';
-                container.style.alignItems = 'center';
-                container.style.justifyContent = 'center';
-                container.title = "Changer le fond de carte";
+                const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control layer-toggle-control');
 
-                // Icon (Simple layers icon or similar)
-                container.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" viewBox="0 0 16 16"><path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/></svg>`;
+                const btn = L.DomUtil.create('div', 'layer-toggle-btn', container);
+                btn.title = 'Changer le fond de carte';
+                btn.setAttribute('role', 'button');
+                btn.setAttribute('tabindex', '0');
+                btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#333" viewBox="0 0 16 16"><path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/></svg>`;
 
-                container.onclick = (e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
+                L.DomEvent.on(btn, 'click', (e) => {
+                    L.DomEvent.preventDefault(e);
+                    L.DomEvent.stopPropagation(e);
                     const nextIndex = (this.currentLayerIndex + 1) % this.layers.length;
                     this.setLayer(nextIndex);
-                };
+                });
 
                 return container;
             }
